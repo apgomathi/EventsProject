92c65257d1693e7b2c692ce937ca61ca828fae05
Gomathinayagam Arumugam <Gomathinayagam.A@India-Powai.orioninc.com>
2017-10-10 13:57:25 +0530

    new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { EventAppComponent } from './event-app.component';
+import { NavBarComponent } from './nav/navbar.component'
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventAppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [EventAppComponent]
 })
 export class AppModule { }
---
+++ src/app/event-app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'event-app',
+    template: `
+    <nav-bar></nav-bar>
+    
+    `
+})
+
+export class EventAppComponent {
+
+}
